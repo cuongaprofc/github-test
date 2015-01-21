030ebddd35e69ecd988e5824c7286e79ba169b1b
cuongaprofc <cuong.aprofc@gmail.com>
2015-01-21 15:18:24 +0700

    edit-2222

--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -8,7 +8,7 @@ namespace WindowsFormsApplication1
     static class Program
     {
         //edit-1
-        //edit-2
+        //edit-22222
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
